45ab3fc6443fffe9bf9af459fc87c96e26243715
jason <Jason.jafari.m@gmail.com>
2020-07-19 01:14:18 -0400

    work with cass component

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,11 @@
 import React from "react"
-import styles from './404.module.css'
+import styles from './404.module.scss'
 // import './404.module.css'
 
 export default function Home() {
     return (
         
-        <div>
+        <div className={styles.content}>
             <h1 className={styles.header}>Page Not Found</h1>
             <p className={styles.errorMessage}>
             Page Not Found
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,23 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import styles from './404.module.scss'
 export default function Home() {
-    return (
-        
-        <div>
-            <h1>About Us</h1>
-            <div>
-            <Link to="/">Home</Link> | <Link to="/about">About me</Link>
-            </div>
-            <p>
-            About Us
+  return (
+
+    <div>
+      <h1>About Us</h1>
+      <div>
+        <Link to="/">Home</Link> | <Link to="/about">About me</Link>
+      </div>
+      <p>
+        About Us
             </p>
-        </div>
-    )
+      <div className={styles.content}>
+        <h1 className={styles.header}>Page Not Found</h1>
+        <p className={styles.errorMessage}>
+          Page Not Found
+            </p>
+      </div>
+    </div>
+  )
 }
